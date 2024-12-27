4a4d47b210122f6a5945681b9aebc934b14cac59
viethq00 <73158024+viethq00@users.noreply.github.com>
2024-12-27 16:14:55 +0700

    Update RomanToInteger.js

--- easy/RomanToInteger.js
+++ easy/RomanToInteger.js
@@ -1,18 +1,26 @@
-// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+function romanToInt(input) {
+  let sumOfRoman = 0;
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
 
-// Symbol       Value
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
-// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+  for (let i = 0; i < input.length - 1; i++) {
+    if (roman[input[i]] < roman[input[i + 1]]) {
+      sumOfRoman -= roman[input[i]];
+      console.log("sumOfRoman minus: ", sumOfRoman);
+    } else {
+      sumOfRoman += roman[input[i]];
+      console.log("sumOfRoman add: ", sumOfRoman);
+    }
+  }
 
-// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+  return sumOfRoman + roman[input[input.length - 1]];
+}
 
-// I can be placed before V (5) and X (10) to make 4 and 9.
-// X can be placed before L (50) and C (100) to make 40 and 90.
-// C can be placed before D (500) and M (1000) to make 400 and 900.
-// Given a roman numeral, convert it to an integer.
+console.log(romanToInt("MCM"));
